e9979a214cd8dda9c22234f374698da23cef4df2
Ernest Wozniak <erniwozniak@gmail.com>
2024-10-29 20:54:25 +0100

    Update index.js
    Final version

--- js/index.js
+++ js/index.js
@@ -18,3 +18,35 @@
 
 // Zadanie opcjonalne: Zastanów się, co się stanie, jeśli nie zostanie przesłany żaden obraz, a przycisk odcieni szarości zostanie kliknięty.
 // Wskazówka: Możesz sprawdzić, czy obraz został przesłany, zanim zastosujesz filtr odcieni szarości.
+var iU = document.getElementById("imageUpload")
+var cG = document.getElementById("convertGrayscale")
+var uI = document.getElementById("uploadedImage")
+var gi = document.getElementById("grayscaleImage")
+const ctx = gi.getContext("2d")
+
+iU.addEventListener("change", function (e) {
+	const file = e.target.files[0]
+	if (file) {
+		const reader = new FileReader()
+		reader.onload = function (e) {
+			uI.src = e.target.result
+		}
+		reader.readAsDataURL(file)
+	}
+})
+cG.addEventListener("click", function (e) {
+	ctx.drawImage(uI, 0, 0, gi.width, gi.height)
+	const imageData = ctx.getImageData(0, 0, gi.width, gi.height)
+	const data = imageData.data
+
+	for (let i = 0; i < data.length; i += 4) {
+		const r = data[i]
+		const g = data[i + 1]
+		const b = data[i + 2]
+		const gray = 0.299 * r + 0.587 * g + 0.114 * b
+		data[i] = gray
+		data[i + 1] = gray
+		data[i + 2] = gray
+	}
+	ctx.putImageData(imageData, 0, 0)
+})
